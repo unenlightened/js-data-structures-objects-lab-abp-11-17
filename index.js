823ebaf128bc7a8f2eb74c3203682ecc50500db0
Chely Ho <adept_inept@yahoo.com>
2017-11-06 21:20:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 // Write your solution in this file!
 const driver = {};
 
+function updateDriverWithKeyAndValue(driver, key, value){
+  return Object.assign({}, driver, {[key]: value});
+}
+
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
   driver[key] = value;
   return driver;
 }
 
-function updateDriverWithKeyAndValue(driver, key, value){
-  return Object.assign({}, driver, {[key]: value});
+function deleteFromDriverByKey(driver, key){
+  
 }
