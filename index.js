7b47ee44990a4cdec1c99a9691c4b32a27df74b9
Chely Ho <adept_inept@yahoo.com>
2017-11-06 20:46:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Write your solution in this file!
 const driver = {};
 
-function updateDriverWithKeyAndValue(driver, key, value){
+function updatedriverWithKeyAndValue(driver, key, value){
   const newDriver = {...driver};
   newDriver[key] = value;
   return newDriver;
 }
 
-function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+function destructivelyUpdatedriverWithKeyAndValue(driver, key, value){
   driver[key] = value;
   return driver;
 }
